8acd02caf5b1aec5fb9cf8de04e25a54c0ef5fb4
JT Wellspring <jtwellspring12@gmail.com>
2025-01-09 12:41:17 -0500

    Chnages to dashboard for intial setup

--- src/app/(components)/Navbar/index.tsx
+++ src/app/(components)/Navbar/index.tsx
@@ -1,16 +1,26 @@
 "use client";
 
-import { Settings, Sun, Bell, Menu } from "lucide-react";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsSidebarCollapsed } from "@/state";
+import { Search, Settings, Sun, Bell, Menu } from "lucide-react";
 import React from "react";
 
 const Navbar = () => {
+      const disbatch = useAppDispatch();
+      const isSidebarCollapsed = useAppSelector(
+        (state) => state.global.isSidebarCollapsed
+      );
+    
+      const toggleSidebar = () => {
+        disbatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+      };
     return (
         <div className="flex justify-between items-center w-full mb-7">
             {/* left side */}
             <div className="flex justify-between items-center gap-5">
                 <button
                     className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
-                    onClick={() => {}}
+                    onClick={toggleSidebar}
                 >
                     <Menu className="w-4 h-4" />
                 </button>
@@ -21,7 +31,7 @@ const Navbar = () => {
                         className="pl-10 pr-4 py-2 w-50 md:w-80 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
                     />
                     <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                        <Bell className="text-gray-500" size={20} />
+                        <Search className="text-gray-500" size={20} />
                     </div>
                 </div>
             </div>
